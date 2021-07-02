aa4dcef0995a0839ca2c400576455d6951266206
Loistracy Andrew <48063656+Loistracy@users.noreply.github.com>
2021-07-02 12:53:29 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+#name = input("what is ur name?  ")
+#print("hello "  +  name)
+
+#name = input("what is ur name? ")
+#color = input("what is ur fav color? ")
+#print(name  + " likes " + color )
+
+#weight = input("whats the weight bruhh? ")
+#real_weight = str(float(weight) / 2.5)+ "is the weight"
+#print(real_weight)
+
+#name = "Jeniffer"
+#print(name[1:-1])
+
+#fname = "tracy"
+#lname = "joseph"
+
+#msg = f'{fname} [{lname}]'
+#print(msg)
+
+#course = "loistracy"
+#print(len(course))
+#print(course.upper())
+
+#print ("lois".upper())
+
+for X in range (0,5):
+X++
+print(X)
